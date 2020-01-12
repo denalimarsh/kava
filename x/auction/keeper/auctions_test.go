41da24c39ebb8feb3f067b36ba1a90c79cdb5e63
denalimarsh <denalimarsh@gmail.com>
2020-01-13 00:38:02 +0100

    Moving bidding tests from types > keeper

--- x/auction/keeper/auctions_test.go
+++ x/auction/keeper/auctions_test.go
@@ -113,7 +113,7 @@ func TestStartForwardAuction(t *testing.T) {
 }
 
 func TestExecuteBasicForwardAuction(t *testing.T) {
-	// Setup
+	// Set up
 	_, addrs := app.GeneratePrivKeyAddressPairs(1)
 	buyer := addrs[0]
 	sellerModName := liquidator.ModuleName
@@ -121,7 +121,7 @@ func TestExecuteBasicForwardAuction(t *testing.T) {
 
 	tApp := app.NewTestApp()
 
-	sellerAcc := supply.NewEmptyModuleAccount(sellerModName, supply.Burner) // forward auctions burn proceeds
+	sellerAcc := supply.NewEmptyModuleAccount(sellerModName, supply.Burner) // Forward auctions burn proceeds
 	require.NoError(t, sellerAcc.SetCoins(cs(c("token1", 100), c("token2", 100))))
 	tApp.InitializeFromGenesisStates(
 		NewAuthGenStateFromAccs(authexported.GenesisAccounts{
@@ -152,8 +152,142 @@ func TestExecuteBasicForwardAuction(t *testing.T) {
 	tApp.CheckBalance(t, ctx, buyer, cs(c("token1", 120), c("token2", 90)))
 }
 
+// TODO: Implement TestForwardAuctionBidding with error edge cases
 func TestForwardAuctionBidding(t *testing.T) {
-	// TODO: Implement TestForwardAuctionBidding with error edge cases
+	buyer1 := sdk.AccAddress([]byte("buyer1"))
+	buyer2 := sdk.AccAddress([]byte("buyer2"))
+	now := time.Now()
+	end := now.Add(1000000)
+
+	type args struct {
+		currentBlockHeight time.Time
+		bidder             sdk.AccAddress
+		lot                sdk.Coin
+		bid                sdk.Coin
+	}
+	tests := []struct {
+		name    string
+		auction types.ForwardAuction
+		args    args
+		// TODO: update
+		// expectedOutputs []BankOutput
+		// expectedInputs  []BankInput
+		expectedEndTime time.Time
+		expectedBidder  sdk.AccAddress
+		expectedBid     sdk.Coin
+		expectpass      bool
+	}{
+		{
+			"normal",
+			types.ForwardAuction{types.BaseAuction{
+				Initiator:  liquidator.ModuleName,
+				Lot:        c("usdx", 100),
+				Bidder:     buyer1,
+				Bid:        c("kava", 6),
+				EndTime:    end,
+				MaxEndTime: end,
+			}},
+			args{now, buyer2, c("usdx", 100), c("kava", 10)},
+			// []BankOutput{{buyer2, c("kava", 10)}},
+			// []BankInput{{buyer1, c("kava", 6)}, {seller, c("kava", 4)}},
+			now + defaultMaxBidDuration,
+			buyer2,
+			c("kava", 10),
+			true,
+		},
+		// {
+		// 	"lowBid",
+		// 	ForwardAuction{BaseAuction{
+		// 		Initiator:  seller,
+		// 		Lot:        c("usdx", 100),
+		// 		Bidder:     buyer1,
+		// 		Bid:        c("kava", 6),
+		// 		EndTime:    end,
+		// 		MaxEndTime: end,
+		// 	}},
+		// 	args{now, buyer2, c("usdx", 100), c("kava", 5)},
+		// 	[]BankOutput{},
+		// 	[]BankInput{},
+		// 	end,
+		// 	buyer1,
+		// 	c("kava", 6),
+		// 	false,
+		// },
+		// {
+		// 	"equalBid",
+		// 	ForwardAuction{BaseAuction{
+		// 		Initiator:  seller,
+		// 		Lot:        c("usdx", 100),
+		// 		Bidder:     buyer1,
+		// 		Bid:        c("kava", 6),
+		// 		EndTime:    end,
+		// 		MaxEndTime: end,
+		// 	}},
+		// 	args{now, buyer2, c("usdx", 100), c("kava", 6)},
+		// 	[]BankOutput{},
+		// 	[]BankInput{},
+		// 	end,
+		// 	buyer1,
+		// 	c("kava", 6),
+		// 	false,
+		// },
+		// {
+		// 	"timeout",
+		// 	ForwardAuction{BaseAuction{
+		// 		Initiator:  seller,
+		// 		Lot:        c("usdx", 100),
+		// 		Bidder:     buyer1,
+		// 		Bid:        c("kava", 6),
+		// 		EndTime:    end,
+		// 		MaxEndTime: end,
+		// 	}},
+		// 	args{end + 1, buyer2, c("usdx", 100), c("kava", 10)},
+		// 	[]BankOutput{},
+		// 	[]BankInput{},
+		// 	end,
+		// 	buyer1,
+		// 	c("kava", 6),
+		// 	false,
+		// },
+		// {
+		// 	"hitMaxEndTime",
+		// 	ForwardAuction{BaseAuction{
+		// 		Initiator:  seller,
+		// 		Lot:        c("usdx", 100),
+		// 		Bidder:     buyer1,
+		// 		Bid:        c("kava", 6),
+		// 		EndTime:    end,
+		// 		MaxEndTime: end,
+		// 	}},
+		// 	args{end - 1, buyer2, c("usdx", 100), c("kava", 10)},
+		// 	[]BankOutput{{buyer2, c("kava", 10)}},
+		// 	[]BankInput{{buyer1, c("kava", 6)}, {seller, c("kava", 4)}},
+		// 	end, // end time should be capped at MaxEndTime
+		// 	buyer2,
+		// 	c("kava", 10),
+		// 	true,
+		// },
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// update auction and return in/outputs
+			outputs, inputs, err := tc.auction.PlaceBid(tc.args.currentBlockHeight, tc.args.bidder, tc.args.lot, tc.args.bid)
+
+			// check for err
+			if tc.expectpass {
+				require.Nil(t, err)
+			} else {
+				require.NotNil(t, err)
+			}
+			// check for correct in/outputs
+			// require.Equal(t, tc.expectedOutputs, outputs)
+			// require.Equal(t, tc.expectedInputs, inputs)
+			// check for correct EndTime, bidder, bid
+			require.Equal(t, tc.expectedEndTime, tc.auction.EndTime)
+			require.Equal(t, tc.expectedBidder, tc.auction.Bidder)
+			require.Equal(t, tc.expectedBid, tc.auction.Bid)
+		})
+	}
 }
 
 // ----------------------------------------------------
@@ -304,9 +438,143 @@ func TestExecuteBasicReverseAuction(t *testing.T) {
 	tApp.CheckBalance(t, ctx, seller, cs(c("token1", 80), c("token2", 110)))
 }
 
-func TestReverseAuctionBidding(t *testing.T) {
-	// TODO: Implement TestReverseAuctionBidding with error edge cases
-}
+// TODO: Implement TestReverseAuctionBidding with error edge cases
+// func TestReverseAuctionBidding(t *testing.T) {
+// 	buyer := sdk.AccAddress([]byte("a_buyer"))
+// 	seller1 := sdk.AccAddress([]byte("seller1"))
+// 	seller2 := sdk.AccAddress([]byte("seller2"))
+// 	end := EndTime(10000)
+// 	now := EndTime(10)
+
+// 	type args struct {
+// 		currentBlockHeight EndTime
+// 		bidder             sdk.AccAddress
+// 		lot                sdk.Coin
+// 		bid                sdk.Coin
+// 	}
+// 	tests := []struct {
+// 		name            string
+// 		auction         ReverseAuction
+// 		args            args
+// 		expectedOutputs []BankOutput
+// 		expectedInputs  []BankInput
+// 		expectedEndTime EndTime
+// 		expectedBidder  sdk.AccAddress
+// 		expectedLot     sdk.Coin
+// 		expectpass      bool
+// 	}{
+// 		{
+// 			"normal",
+// 			ReverseAuction{BaseAuction{
+// 				Initiator:  buyer,
+// 				Lot:        c("kava", 10),
+// 				Bidder:     seller1,
+// 				Bid:        c("usdx", 100),
+// 				EndTime:    end,
+// 				MaxEndTime: end,
+// 			}},
+// 			args{now, seller2, c("kava", 9), c("usdx", 100)},
+// 			[]BankOutput{{seller2, c("usdx", 100)}},
+// 			[]BankInput{{seller1, c("usdx", 100)}, {buyer, c("kava", 1)}},
+// 			now + DefaultMaxBidDuration,
+// 			seller2,
+// 			c("kava", 9),
+// 			true,
+// 		},
+// 		{
+// 			"highBid",
+// 			ReverseAuction{BaseAuction{
+// 				Initiator:  buyer,
+// 				Lot:        c("kava", 10),
+// 				Bidder:     seller1,
+// 				Bid:        c("usdx", 100),
+// 				EndTime:    end,
+// 				MaxEndTime: end,
+// 			}},
+// 			args{now, seller2, c("kava", 11), c("usdx", 100)},
+// 			[]BankOutput{},
+// 			[]BankInput{},
+// 			end,
+// 			seller1,
+// 			c("kava", 10),
+// 			false,
+// 		},
+// 		{
+// 			"equalBid",
+// 			ReverseAuction{BaseAuction{
+// 				Initiator:  buyer,
+// 				Lot:        c("kava", 10),
+// 				Bidder:     seller1,
+// 				Bid:        c("usdx", 100),
+// 				EndTime:    end,
+// 				MaxEndTime: end,
+// 			}},
+// 			args{now, seller2, c("kava", 10), c("usdx", 100)},
+// 			[]BankOutput{},
+// 			[]BankInput{},
+// 			end,
+// 			seller1,
+// 			c("kava", 10),
+// 			false,
+// 		},
+// 		{
+// 			"timeout",
+// 			ReverseAuction{BaseAuction{
+// 				Initiator:  buyer,
+// 				Lot:        c("kava", 10),
+// 				Bidder:     seller1,
+// 				Bid:        c("usdx", 100),
+// 				EndTime:    end,
+// 				MaxEndTime: end,
+// 			}},
+// 			args{end + 1, seller2, c("kava", 9), c("usdx", 100)},
+// 			[]BankOutput{},
+// 			[]BankInput{},
+// 			end,
+// 			seller1,
+// 			c("kava", 10),
+// 			false,
+// 		},
+// 		{
+// 			"hitMaxEndTime",
+// 			ReverseAuction{BaseAuction{
+// 				Initiator:  buyer,
+// 				Lot:        c("kava", 10),
+// 				Bidder:     seller1,
+// 				Bid:        c("usdx", 100),
+// 				EndTime:    end,
+// 				MaxEndTime: end,
+// 			}},
+// 			args{end - 1, seller2, c("kava", 9), c("usdx", 100)},
+// 			[]BankOutput{{seller2, c("usdx", 100)}},
+// 			[]BankInput{{seller1, c("usdx", 100)}, {buyer, c("kava", 1)}},
+// 			end, // end time should be capped at MaxEndTime
+// 			seller2,
+// 			c("kava", 9),
+// 			true,
+// 		},
+// 	}
+// 	for _, tc := range tests {
+// 		t.Run(tc.name, func(t *testing.T) {
+// 			// update auction and return in/outputs
+// 			outputs, inputs, err := tc.auction.PlaceBid(tc.args.currentBlockHeight, tc.args.bidder, tc.args.lot, tc.args.bid)
+
+// 			// check for err
+// 			if tc.expectpass {
+// 				require.Nil(t, err)
+// 			} else {
+// 				require.NotNil(t, err)
+// 			}
+// 			// check for correct in/outputs
+// 			require.Equal(t, tc.expectedOutputs, outputs)
+// 			require.Equal(t, tc.expectedInputs, inputs)
+// 			// check for correct EndTime, bidder, bid
+// 			require.Equal(t, tc.expectedEndTime, tc.auction.EndTime)
+// 			require.Equal(t, tc.expectedBidder, tc.auction.Bidder)
+// 			require.Equal(t, tc.expectedLot, tc.auction.Lot)
+// 		})
+// 	}
+// }
 
 // ----------------------------------------------------
 // 					Reverse Auctions
@@ -375,6 +643,117 @@ func TestExecuteBasicForwardReverseAuction(t *testing.T) {
 	tApp.CheckBalance(t, ctx, buyer, cs(c("token1", 115), c("token2", 50)))
 }
 
-func TestForwardReverseAuctionBidding(t *testing.T) {
-	// TODO: Implement TestForwardReverseAuctionBidding with error edge cases
-}
+// TODO: Implement TestForwardReverseAuctionBidding with error edge cases
+// func TestForwardReverseAuction_PlaceBid(t *testing.T) {
+// 	cdpOwner := sdk.AccAddress([]byte("a_cdp_owner"))
+// 	seller := sdk.AccAddress([]byte("a_seller"))
+// 	buyer1 := sdk.AccAddress([]byte("buyer1"))
+// 	buyer2 := sdk.AccAddress([]byte("buyer2"))
+// 	end := EndTime(10000)
+// 	now := EndTime(10)
+
+// 	type args struct {
+// 		currentBlockHeight EndTime
+// 		bidder             sdk.AccAddress
+// 		lot                sdk.Coin
+// 		bid                sdk.Coin
+// 	}
+// 	tests := []struct {
+// 		name            string
+// 		auction         ForwardReverseAuction
+// 		args            args
+// 		expectedOutputs []BankOutput
+// 		expectedInputs  []BankInput
+// 		expectedEndTime EndTime
+// 		expectedBidder  sdk.AccAddress
+// 		expectedLot     sdk.Coin
+// 		expectedBid     sdk.Coin
+// 		expectpass      bool
+// 	}{
+// 		{
+// 			"normalForwardBid",
+// 			ForwardReverseAuction{BaseAuction: BaseAuction{
+// 				Initiator:  seller,
+// 				Lot:        c("xrp", 100),
+// 				Bidder:     buyer1,
+// 				Bid:        c("usdx", 5),
+// 				EndTime:    end,
+// 				MaxEndTime: end},
+// 				MaxBid:      c("usdx", 10),
+// 				OtherPerson: cdpOwner,
+// 			},
+// 			args{now, buyer2, c("xrp", 100), c("usdx", 6)},
+// 			[]BankOutput{{buyer2, c("usdx", 6)}},
+// 			[]BankInput{{buyer1, c("usdx", 5)}, {seller, c("usdx", 1)}},
+// 			now + DefaultMaxBidDuration,
+// 			buyer2,
+// 			c("xrp", 100),
+// 			c("usdx", 6),
+// 			true,
+// 		},
+// 		{
+// 			"normalSwitchOverBid",
+// 			ForwardReverseAuction{BaseAuction: BaseAuction{
+// 				Initiator:  seller,
+// 				Lot:        c("xrp", 100),
+// 				Bidder:     buyer1,
+// 				Bid:        c("usdx", 5),
+// 				EndTime:    end,
+// 				MaxEndTime: end},
+// 				MaxBid:      c("usdx", 10),
+// 				OtherPerson: cdpOwner,
+// 			},
+// 			args{now, buyer2, c("xrp", 99), c("usdx", 10)},
+// 			[]BankOutput{{buyer2, c("usdx", 10)}},
+// 			[]BankInput{{buyer1, c("usdx", 5)}, {seller, c("usdx", 5)}, {cdpOwner, c("xrp", 1)}},
+// 			now + DefaultMaxBidDuration,
+// 			buyer2,
+// 			c("xrp", 99),
+// 			c("usdx", 10),
+// 			true,
+// 		},
+// 		{
+// 			"normalReverseBid",
+// 			ForwardReverseAuction{BaseAuction: BaseAuction{
+// 				Initiator:  seller,
+// 				Lot:        c("xrp", 99),
+// 				Bidder:     buyer1,
+// 				Bid:        c("usdx", 10),
+// 				EndTime:    end,
+// 				MaxEndTime: end},
+// 				MaxBid:      c("usdx", 10),
+// 				OtherPerson: cdpOwner,
+// 			},
+// 			args{now, buyer2, c("xrp", 90), c("usdx", 10)},
+// 			[]BankOutput{{buyer2, c("usdx", 10)}},
+// 			[]BankInput{{buyer1, c("usdx", 10)}, {cdpOwner, c("xrp", 9)}},
+// 			now + DefaultMaxBidDuration,
+// 			buyer2,
+// 			c("xrp", 90),
+// 			c("usdx", 10),
+// 			true,
+// 		},
+// 		// TODO more test cases
+// 	}
+// 	for _, tc := range tests {
+// 		t.Run(tc.name, func(t *testing.T) {
+// 			// update auction and return in/outputs
+// 			outputs, inputs, err := tc.auction.PlaceBid(tc.args.currentBlockHeight, tc.args.bidder, tc.args.lot, tc.args.bid)
+
+// 			// check for err
+// 			if tc.expectpass {
+// 				require.Nil(t, err)
+// 			} else {
+// 				require.NotNil(t, err)
+// 			}
+// 			// check for correct in/outputs
+// 			require.Equal(t, tc.expectedOutputs, outputs)
+// 			require.Equal(t, tc.expectedInputs, inputs)
+// 			// check for correct EndTime, bidder, bid
+// 			require.Equal(t, tc.expectedEndTime, tc.auction.EndTime)
+// 			require.Equal(t, tc.expectedBidder, tc.auction.Bidder)
+// 			require.Equal(t, tc.expectedLot, tc.auction.Lot)
+// 			require.Equal(t, tc.expectedBid, tc.auction.Bid)
+// 		})
+// 	}
+// }
